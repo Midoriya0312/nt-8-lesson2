a86fcbdb4b8567d94e4a46e9102f0b56e908a8db
Midoriya0312 <oybektojiboyev0312@gmail.com>
2023-06-21 11:25:59 +0500

    lesson2

---
+++ main.js
@@ -0,0 +1,224 @@
+// var a = 4;
+
+// if(a % 2 != 0 && a >= 0 || a % 2 == 0 && a >= 0){
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+
+// var age = 16;
+// ternary operators
+// var res = age >= 18 ? 'can vote' : 'can not';
+
+// var condition = age > 15;
+// var condition2 = age > 18;
+// var value = 'can vote';
+// var value2 = 'can not';
+// var value3 = 'too young';
+
+// var res = condition2 ? value : condition ? value2 : value3;
+// console.log(res);
+
+
+
+// ternary homework
+
+
+// 1 misol
+
+// var a = 1;
+
+// var res = a >= 0 ? 'true' : 'false';
+
+// console.log(res);
+
+// 2,3 misol
+
+// var n = 11;
+
+// var res = n % 2 !== 0 ? 'toq son' : 'juft son';
+
+// console.log(res);
+
+// 4 misol
+
+// var a = 3;
+// var b = 5;
+
+// var res = a > 0 && b > 0 ? 'true' : 'false';
+
+// console.log(res);
+
+// 5 misol
+
+// var a = 5;
+// var b = 3;
+// var c = 1;
+
+// var res = a > b && b > c ? 'true' : 'false';
+// console.log(res);
+
+// 6 misol
+
+// var a = 1;
+// var b = 8;
+// var c = 6;
+
+// var res = a < b && b > c ? 'true' : 'false';
+// console.log(res);
+
+// 7 misol
+
+// var a = 11;
+// var b = 11;
+
+// var res = a % 2 !== 0 && b & 2 !== 0 ? 'true' : 'false';
+// console.log(res);
+
+// 8 misol
+
+// var a = 3;
+// var b = 4;
+// var c = 1;
+// var d = 7;
+
+// var res = a >= 0 && b >= 0 && c >= 0 && d >= 0 ? 'true' : 'false';
+// console.log(res);
+
+// 9 misol
+
+// var a = 4;
+// var b = -2;
+// var c = 5;
+
+// var res = a >= 0 && b >= 0 || b >= 0 && c >= 0 || a >= 0 && c >= 0 ? 'true' : 'false';
+// console.log(res);
+
+// 10 misol
+
+// var a = 4;
+// var condition = a == 1;
+// var condition2 = a == 2;
+// var condition3 = a == 3;
+// var condition4 = a == 4;
+// var condition5 = a == 5;
+// var condition6 = a == 6;
+// var condition7 = a == 7;
+// var value = 'dushanba';
+// var value2 = 'seshanba';
+// var value3 = 'chorshanba';
+// var value4 = 'payshanba';
+// var value5 = 'juma';
+// var value6 = 'shanba';
+// var value7 = 'yakshanba';
+// var value8 = 'bunaqa kun yoq'; 
+
+// var res = condition ? value : condition2 ? value2 : condition3 ? value3 : condition4 ? value4 : condition5 ? value5 : condition6 ? value6 : condition7 ? value7 : value8;
+// console.log(res);
+
+// 11 misol
+
+// var a = 91;
+
+// var condition = a == 99;
+// var condition2 = a == 90 || a == 91;
+// var condition3 = a == 71;
+// var condition4 = a == 98;
+// var condition5 = a == 94;
+// var value = 'uzmobile';
+// var value2 = 'beeline';
+// var value3 = 'Gorodskoy';
+// var value4 = 'perfectum';
+// var value5 = 'usell';
+// var value1 = 'false';
+
+// var res = condition ? value : condition2 ? value2 : condition3 ? value3 : condition4 ? value4 : condition5 ? value5 : value1;
+
+// console.log(res);
+
+// 12 misol
+
+// var a = -1;
+
+// var res = a >= 0 ? a++ : a--;
+
+// console.log(res);
+
+// 13 misol
+
+// var a = -3;
+
+// var res = a >= 0 ? a += 3 : a -= 2;
+// console.log(res);
+
+// 14 misol
+
+// var a = 7;
+// var b = 5;
+
+// var res = a > b ? a : b > a ? b : 'false';
+
+// console.log(res);
+
+// 15 misol
+
+// var a = 4;
+// var b = 1;
+// var c = 5;
+
+// var condition = a > b && a > c;
+// var condition2 = b > a && b > c;
+// var condition3 = c > a && c > b;
+
+// var res = condition ? a : condition2 ? b : condition3 ? c : 'false';
+
+// console.log(res);
+
+// 16 misol
+
+// var a = 4;
+// var b = 1;
+// var c = 5;
+
+// var condition = a < b && a < c;
+// var condition2 = b < a && b < c;
+// var condition3 = c < a && c < b;
+
+// var res = condition ? a : condition2 ? b : condition3 ? c : 'false';
+// console.log(res);
+
+// 17 misol
+
+// var a = 'male';
+
+// var res = a == 'female' ? 'siz qiz bolasiz' : 'siz oqil bolasiz';
+// console.log(res);
+
+// 18 misol
+
+// var a = 5;
+
+// var condition = a == 5;
+// var condition2 = a == 4;
+// var value = 'siz stependiya ololisiz';
+// var value2 = 'siz stependiya ololisiz';
+// var value1 = 'stependiya olomisiz';
+
+// var res = condition ? value : condition2 ? value2 :  value1;
+// console.log(res);
+
+// 19 misol yeshilmadi
+
+
+
+
+
+// 20 misol yechilmadi
+
+// var a = 2006;
+// var b = 2023;
+
+// var c = b - a;
+
+// var res = c ? c;
